d4d8f0cbfea41f7328fdef812c883be318645e97
Jeremy Koerber <jeremykoerber@users.noreply.github.com>
2023-01-31 11:58:23 -0800

    move backend to s3

--- backend.tf
+++ backend.tf
@@ -1,13 +1,21 @@
 # The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
+# terraform {
+#   backend "remote" {
+#     organization = "caylent-jkoerber"
+
+#     workspaces {
+#       name = "tfc-guide-example"
+#     }
+#   }
+
+#   required_version = ">= 0.14.0"
+# }
+
 terraform {
-  backend "remote" {
-    organization = "caylent-jkoerber"
-
-    workspaces {
-      name = "tfc-guide-example"
-    }
+  backend "s3" {
+    bucket = "jkoerber-terraform-test"
+    key    = "sv-K5jhAiKhVNf3UfHC.tfstate"
+    region = "us-east-1"
   }
-
-  required_version = ">= 0.14.0"
 }
